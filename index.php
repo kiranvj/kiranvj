2c8fdda66cab940bcde69ea2118153867ea0d25a
kiranvj <kiranvj@gmail.com>
2017-12-22 23:06:52 -0500

    from codeanywhere

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Kiran Paul VJ  &lt; Computer Engineer /&gt;</title>
+<link href="css/styles.css?v=1" rel="stylesheet" type="text/css"></link>
+</head>
+<body>
+	<div class="page">
+		<h1>Kiran Paul VJ</h1>
+		<h2>Computer Engineer</h2>
+		<section>
+			<h1>About</h1>
+			<p>I am a Dallas based computer engineer interested in HTML, JavaScript, CSS, UI/UX, SVG and data visualization. Current I am working at Cognizant Technology Solutions as Technology Specialist.</p>
+			<p>Recently I am more into HTML5, CSS, JavaScript and mobile development. I have worked in ASP, PHP, MFC, VisualBasic, ActionScript, Photoshop (from v4.0), CorelDraw, Adobe Flash (from v3.0[then Macromedia]).</p>
+
+			<p>Uses PHP for personal projects. Once in a while refreshes Photoshop, Coreldraw and Flash. I like electronics, physics, arts, raspberryPi, circuit design, grey hat, easter eggs, beaches, country side ...</p>
+		</section>
+		<!-- SO flair -->
+		<div class="text-center">
+			<a href="https://stackexchange.com/users/1221815">
+				<img src="https://stackexchange.com/users/flair/1221815.png" width="208" height="58" alt="profile for kiranvj on Stack Exchange, a network of free, community-driven Q&amp;A sites" title="profile for kiranvj on Stack Exchange, a network of free, community-driven Q&amp;A sites">
+			</a>
+		</div>
+		<!-- end: SO flair -->
+		<!-- EE badge -->
+		include("ee-badge.php");
+		<!-- end: EE badge -->
+	</div>
+</body>
+</html>
